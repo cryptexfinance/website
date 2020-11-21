4bc86080a9db5af0114a93ed71b2cb3b94240b49
Eduardo Fonseca <edsphinx@gmail.com>
2020-11-21 05:49:43 -0600

    blog fix

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import useSiteMetadata from './SiteMetadata'
+import { withPrefix } from 'gatsby'
+
+const TemplateWrapper = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="business.business" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="/" />
+        <meta
+          property="og:image"
+          content={`${withPrefix('/')}img/og-image.jpg`}
+        />
+      </Helmet>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default TemplateWrapper
