213a21ad934bdc3fe78484224ae551ead52bb173
Eduardo Fonseca <edsphinx@gmail.com>
2020-11-28 21:34:51 -0600

    responsive menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,104 @@
-import React from "react"
+import React,{ useEffect, useState } from "react"
 // import ThemeContext from "../utils/theme"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "gatsby"
-// import "./Fontawesome.js"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { image } from ""
 
 export default () => {
+  const mediaQuery = "screen and (min-width: 768px)";
+  const mql = window.matchMedia(mediaQuery);
+  const [showDesktopMenu, setShowDesktopMenu] = useState(mql.matches);
+
+  useEffect(() => {
+    const handleMediaChange = (media) => {
+      setShowDesktopMenu(media.matches);
+    };
+
+    mql.addEventListener("change", handleMediaChange);
+    setShowDesktopMenu(mql.matches);
+
+    return () => {
+      mql.removeEventListener("change", handleMediaChange);
+    };
+  }, [mql]);
   // const { toString } = useContext(ThemeContext)
-  return (
-    <Navbar variant={toString()} fixed="top" collapseOnSelect expand="md">
-      <Navbar.Brand className="pl-5 ml-5" as={Link} to="/">
-        <img className="menu-logo" src="/logo.svg" alt="Logo" />
-      </Navbar.Brand>
-      <Navbar.Collapse
-        id="responsive-navbar-nav"
-        className="justify-content-end"
-      >
-        <Nav className="nav-links">
-          <Nav.Link as={Link} to="#about" className="nav-links" activeClassName="nav-active" title="What is TCAP?">
-            What is TCAP?
-          </Nav.Link>
-          <Nav.Link as={Link} to="#features" title="How to use TCAP?">
-            How to use TCAP?
-          </Nav.Link>
-          <Nav.Link as={Link} to="#team" title="Team">
-            Team
-          </Nav.Link>
-          <Nav.Link as={Link} to="#faq" title="FAQ">
-            FAQ
-          </Nav.Link>
-          <Nav.Link as={Link} to="#community" title="Comunity">
-            Community
-          </Nav.Link>
+  // const isMobile = width <= 500;
+
+  const NavMobile = () => {
+    return (
+      <>
+        <Navbar fixed="top" collapseOnSelect expand="sm">
+          <Navbar.Brand className="pl-5 ml-5" as={Link} to="/">
+            <img className="menu-logo" src="/logom.svg" alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="mobile_menu_bar" aria-hidden="true"/>
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Navbar.Text>Menu</Navbar.Text>
+            <Nav className="nav-links">
+              <Nav.Link as={Link} to="#about" className="nav-links" title="What is TCAP?">
+                What is TCAP?
+              </Nav.Link>
+              <Nav.Link as={Link} to="#features" title="How to use TCAP?">
+                How to use TCAP?
+              </Nav.Link>
+              <Nav.Link as={Link} to="#team" title="Team">
+                Team
+              </Nav.Link>
+              <Nav.Link as={Link} to="#faq" title="FAQ">
+                FAQ
+              </Nav.Link>
+              <Nav.Link as={Link} to="#community" title="Comunity">
+                Community
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Navbar fixed="top" collapseOnSelect expand="sm" className="responsive-app-button">
           <a href="https://app.cryptex.finance" target="_blank" rel="noreferrer" className="button-navbar">
             Go to App
           </a>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+        </Navbar>
+      </>
+    )
+  }
+
+  const NavDesktop = () => {
+    return (
+      <Navbar fixed="top" collapseOnSelect expand="lg">
+        <Navbar.Brand className="pl-5 ml-5" as={Link} to="/">
+          <img className="menu-logo" src="/logo.svg" alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="mobile_menu_bar" aria-hidden="true"/>
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="nav-links">
+            <Nav.Link as={Link} to="#about" className="nav-links" title="What is TCAP?">
+              What is TCAP?
+            </Nav.Link>
+            <Nav.Link as={Link} to="#features" title="How to use TCAP?">
+              How to use TCAP?
+            </Nav.Link>
+            <Nav.Link as={Link} to="#team" title="Team">
+              Team
+            </Nav.Link>
+            <Nav.Link as={Link} to="#faq" title="FAQ">
+              FAQ
+            </Nav.Link>
+            <Nav.Link as={Link} to="#community" title="Comunity">
+              Community
+            </Nav.Link>
+            <a href="https://app.cryptex.finance" target="_blank" rel="noreferrer" className="button-navbar">
+              Go to App
+            </a>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+
+  return (
+    <>
+      { showDesktopMenu ? 
+          <NavDesktop/> :
+          <NavMobile/> }
+    </>
   )
 }
