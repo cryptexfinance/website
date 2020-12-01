de671b0ff45332d1035a0c4f92c5392444f16cef
Eduardo Fonseca <edsphinx@gmail.com>
2020-11-30 22:59:17 -0600

    mobile menu updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,24 +32,28 @@ export default () => {
         <Navbar fixed="top" expand="sm">
           <Navbar.Brand className="pl-5 ml-5" as={Link} to="/">
             <img className="menu-logo" src="/logom.svg" alt="Logo" />
+            <div className="menu-logo-divider"></div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" className="mobile_menu_bar" aria-hidden="true"/>
           <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-          <Navbar.Text>Menu</Navbar.Text>
-            <Nav className="nav-links">
+          <Nav className="nav-links">
               <Nav.Link as={Link} to="#about" className="nav-links" title="What is TCAP?">
                 What is TCAP?
               </Nav.Link>
+              <div className="nav-links-divisor"></div>
               <Nav.Link as={Link} to="#features" title="How to use TCAP?">
                 How to use TCAP?
               </Nav.Link>
+              <div className="nav-links-divisor"></div>
               <Nav.Link as={Link} to="#team" title="Team">
                 Team
               </Nav.Link>
+              <div className="nav-links-divisor"></div>
               <Nav.Link as={Link} to="#faq" title="FAQ">
                 FAQ
               </Nav.Link>
-              <Nav.Link as={Link} to="#community" title="Comunity">
+              <div className="nav-links-divisor"></div>
+              <Nav.Link as={Link} to="#community" title="Community">
                 Community
               </Nav.Link>
             </Nav>
