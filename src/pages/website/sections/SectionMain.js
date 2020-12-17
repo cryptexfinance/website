aed1dbe85e74ee0f7e11fec50ee16d3c0736bcf6
Eduardo Fonseca <edsphinx@gmail.com>
2020-12-17 00:50:01 -0600

    fix bad initial text

--- src/pages/website/sections/SectionMain.js
+++ src/pages/website/sections/SectionMain.js
@@ -11,10 +11,11 @@ import gql from 'graphql-tag';
 
 const SectionMain = () => {
   // const appEndpoint = "https://rinkeby.cryptex.finance/";
-  const isMobileDevice = useMediaQuery({ query: '(max-device-width: 375px)' });
-  const isMobileDevice2 = useMediaQuery({ query: '(max-device-width: 390px)' });
-  const isMobileDevice3 = useMediaQuery({ query: '(max-device-width: 414px)' });
-  const isMobileDevice4 = useMediaQuery({ query: '(max-device-width: 428px)' });
+  const isMobileDevice = useMediaQuery({ query: '(max-device-width: 320px)' });
+  const isMobileDevice2 = useMediaQuery({ query: '(max-device-width: 375px)' });
+  const isMobileDevice3 = useMediaQuery({ query: '(max-device-width: 390px)' });
+  const isMobileDevice4 = useMediaQuery({ query: '(max-device-width: 414px)' });
+  const isMobileDevice5 = useMediaQuery({ query: '(max-device-width: 428px)' });
   const isTabletDevice = useMediaQuery({ query: '(max-device-width: 768px)' });
   const isTabletDevice2 = useMediaQuery({ query: '(max-device-width: 834px)' });
   const isTabletDevice3 = useMediaQuery({ query: '(max-device-width: 1024px)' });
@@ -51,7 +52,7 @@ const SectionMain = () => {
   return (
     
     <>
-      {(isMobileDevice || isMobileDevice2 || isMobileDevice3 || isMobileDevice4) && isPortrait ? <div className="main-title header">The World's <br/>First Total Crypto<br/>Market Cap Token</div> : 
+      {(isMobileDevice || isMobileDevice2 || isMobileDevice3 || isMobileDevice4 || isMobileDevice5) && isPortrait ? <div className="main-title header">The World's <br/>First Total Crypto<br/>Market Cap Token</div> : 
       (isTabletDevice || isTabletDevice2 || isTabletDevice3) && isPortrait ? <div className="main-title header">The World's First <br/>Total Crypto Market Cap Token</div> :
       isDesktopOrLaptop && !isPortrait && <div className="main-title header">The World's First <br/>Total Crypto Market Cap Token</div>}
       <div className="main-number-pink">{loading && <p>Loading...</p>} {data && "$" + totalTcapPrice}</div>
