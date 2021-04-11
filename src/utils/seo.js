b986c9fa89115ef37615bd9cd86ec0a7ad95f9c7
Cristian Espinoza <ceegarner@gmail.com>
2021-04-11 13:56:31 -0400

    fixes

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -23,15 +23,16 @@ const SEO = ({ description, lang, meta, image: img, title, pathname }) => {
         { name: `description`, content: metaDescription },
         { name: `keywords`, content: site.siteMetadata.keywords.join(`,`) },
         { property: `og:site_name`, content: "TCAP by Cryptex Finance." },
-        { property: `og:title`, content: title },
+        { property: `og:title`, content: `TCAP - By Cryptex Finance` },
         { property: `og:description`, content: metaDescription },
         { property: `og:type`, content: `website` },
+        { property: `og:url`, content: `https://cryptex.finance` },
         {
           property: "og:image",
           content: "https://ctx-rinkeby.s3.amazonaws.com/TCAP_meta.png",
         },
         { name: `twitter:creator`, content: site.siteMetadata.author },
-        { name: `twitter:title`, content: title },
+        { name: `twitter:title`, content: `TCAP - By Cryptex Finance` },
         { name: `twitter:description`, content: metaDescription },
         {
           name: "twitter:image",
