da09d3de19015106dd95eec802543c0d685adbf0
jdestephen <jdestephen07@gmail.com>
2021-05-14 18:32:53 -0600

    adding new team members

---
+++ src/pages/team/data.js
@@ -0,0 +1,103 @@
+import joe from '../../../static/website/team/joe.webp';
+import preston from '../../../static/website/team/preston.webp';
+import cristian from '../../../static/website/team/cristian.webp';
+import tom from '../../../static/website/team/tom.webp';
+import jorge from '../../../static/website/team/jorge.png';
+import cullen from '../../../static/website/team/cullen.png';
+import james from '../../../static/website/team/james.png';
+import oliver from '../../../static/website/team/oliver.png';
+import brendan from '../../../static/website/team/brendan.png';
+import zak from '../../../static/website/team/zak.png';
+import don from '../../../static/website/team/don.png';
+import joem from '../../../static/website/team/joem.webp';
+import prestonm from '../../../static/website/team/prestonm.webp';
+import cristianm from '../../../static/website/team/cristianm.webp';
+import tomm from '../../../static/website/team/tomm.webp';
+import jorgem from '../../../static/website/team/jorgem.png';
+import cullenm from '../../../static/website/team/cullenm.png';
+import jamesm from '../../../static/website/team/jamesm.png';
+import oliverm from '../../../static/website/team/oliverm.png';
+import brendanm from '../../../static/website/team/brendanm.png';
+import zakm from '../../../static/website/team/zakm.png';
+import donm from '../../../static/website/team/donm.png';
+
+
+export const teamData = [
+    {
+        name: "Joe Sticco",
+        position: "Co-Founder & CEO",
+        description: "A lifelong trader and investment manager, Joseph brings immense knowledge of traditional market structure to the project. He founded Cryptex Finance with the vision of combining Wall Street with world class blockchain development in building state of the art investment solutions for tomorrow.",
+        image: joe,
+        mobile_image: joem    
+    },
+    {
+        name: "Preston Van Loon",
+        position: "Co-Founder & Blockchain Lead",
+        description: "Preston is CEO and co-founder of Prysmatic Labs and leads a team of dedicated blockchain developers in building critical layer one scaling solutions for Ethereum 2.0. Since January 2018, Preston and team have received nearly $2MM in grant funding to pursue these efforts from the Ethereum Foundation, Vitalik Buterin, Ethereum Community Fund, and many other community members.",
+        image: preston,
+        mobile_image: prestonm
+    },
+    {
+        name: "Cristian Espinoza",
+        position: "Blockchain Developer",
+        description: "Smart Contract Developer certified by ConsenSys, Coffee Lover & Prev. Devcon Scholar at Ethereum Foundation. Leader of the Honduras Ethereum Community.",
+        image: cristian,
+        mobile_image: cristianm
+    },
+    {
+        name: "Thomas Matzner",
+        position: "Co-Founder & CBO",
+        description: "Thomas has led planning, branding and marketing initiatives at world class companies and startups. He has taken high volume and celebrity endorsed products from concept to launch with an omnichannel strategy. Thomas' passion is building brands with bold visions of leaving a legacy.",
+        image: tom,
+        mobile_image: tomm
+    },
+    {
+        name: "Jorge",
+        position: "Full Stack Developer",
+        description: "Full Stack developer with more than seven years of experience using technologies like react, react native, python, postgreSQL, among others. Jorge holds a M.S. in Computer Science degree, also he published a paper on the 2015 IEEE International Conference on Big Data.",
+        image: jorge,
+        mobile_image: jorgem
+    },
+    {
+        name: "Cullen",
+        position: "Graphic Designer",
+        description: "Writer, Director, and VFX Artist, who's done VFX for shows like Man in the High Castle, Room 104, and Silicon Valley. Born and raised in the Midwest, Cullen always had a passion for movies and storytelling. After years of studying to be a doctor, he abandoned that dream for another and moved to Hollywood. SCUBA diver, adventure seeker, and salsa dancer.",
+        image: cullen,
+        mobile_image: cullenm
+    },
+    {
+        name: "James",
+        position: "Media Production",
+        description: "A multifaceted media producer, James brings two decades of experience in content creation to the team. As a musician, sound designer, audio engineer, event organizer, and meme page administrator, James is always nurturing his creative instincts and dreaming up interesting ways to tell a story. With a vast network of cohorts, spanning all spectrums of the entertainment industry, the question isn't 'can it be done', its 'what's your budget'.",
+        image: james,
+        mobile_image: jamesm
+    },
+    {
+        name: "Oliver",
+        position: "Music Production",
+        description: "Most likely cuddling with your girlfriend or wife when he isn't making beats.",
+        image: oliver,
+        mobile_image: oliverm
+    },
+    {
+        name: "Brendan",
+        position: "Brand Manager",
+        description: "Brendan builds and nurtures a DeFi community with a passion for education and social media development. A blockchain enthusiast, he is skilled in video editing, enjoys music, and pursues photography.",
+        image: brendan,
+        mobile_image: brendanm
+    },
+    {
+        name: "Zak",
+        position: "Advisor",
+        description: "Co-Founder and CTO, Slingshot. Zak serves as technical advisor to several blockchain related companies such as DeFi Pulse and the Syscoin foundation. Previously Zak founded Whiteblock, which specialized in blockchain protocol testing, research and development.",
+        image: zak,
+        mobile_image: zakm
+    },
+    {
+        name: "Don",
+        position: "Advisor",
+        description: "Co-Founder and Managing Director, Quantstamp. Don is an investor and entrepreneur with global perspectives and cross functional experiences supporting investments, product, and growth. Don previously worked at IDG Ventures and PNP Ventures where he helped launch Nexla, Ayasdi, and the Los Angeles Football Club.",
+        image: don,
+        mobile_image: donm
+    }
+];
