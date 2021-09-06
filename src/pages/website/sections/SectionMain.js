283a48504d2b05ceefd144a41809c5a9990217cf
jdestephen <jdestephen07@gmail.com>
2021-09-06 10:52:04 -0600

    main section

--- src/pages/website/sections/SectionMain.js
+++ src/pages/website/sections/SectionMain.js
@@ -1,6 +1,7 @@
 import React,{ useEffect, useState } from 'react';
 import { BigNumber, utils } from "ethers";
-import { useMediaQuery } from 'react-responsive';
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 import image from '../../../../static/website/home/main.webp';
 import tcap from '../../../../static/website/home/tcap.svg';
 import appEndpoint from '../../../endpoint';
@@ -43,29 +44,38 @@ const SectionMain = () => {
   }, [networkStatus]);  
 
   return ( 
-    <>      
-      <div className="main-title header">The World's First <br/>Total Crypto Market Cap Token</div>
+    <div className="section-main">      
+        <Col xs={12} sm={12} md={12} lg={6} className="main-info">
+          <div className="main-title header">The World's First <br/>Total Crypto Market Cap Token</div>
 
-      <div className="main-number-pink">{loading && firstLoad && <p>Loading...</p>} {data && "$" + totalTcapPrice}</div>
-      
-      <div className="main-bold-text">Total Crypto Market Capitalization</div>
-      <div className="main-divider"></div>
-      
-      <div className="main-number-blue">{loading && firstLoad  && <p>Loading...</p>} {data && "$" + tcapPrice}</div>
-      
-      <div className="main-tcap">
-        <img src={tcap} className="main-tcap-image" alt="TCAP" />
-        <div className="main-tcap-text">&nbsp;TCAP</div>
-      </div>
-      {/* <div className="main-number-green">+7.5%</div> */}
-      <a href={appEndpoint} rel="noopener noreferrer" className="button-pink main-button main-button-link" target="_blank">
-        Go to App
-      </a>
-    
-      <div className="main-image">
-        <img src={image} alt="Main" className="main-image" />
-      </div>
-    </>
+          <Row>
+            <Col md={8} lg={8} className="market-cap">
+              <div className="data">
+                <div className="main-number-pink">{loading && firstLoad && <p>Loading...</p>} {data && "$" + totalTcapPrice}</div>            
+                <div className="main-bold-text">Total Crypto Market Capitalization</div>
+              </div>
+              <div>
+                <div className="main-divider"></div>
+              </div>  
+            </Col>
+            <Col md={4} lg={4} className="tcap-data">
+              <div className="main-number-blue">{loading && firstLoad  && <p>Loading...</p>} {data && "$" + tcapPrice}</div>          
+              <div className="main-tcap">
+                <img src={tcap} className="main-tcap-image" alt="TCAP" />
+                <div className="main-tcap-text">&nbsp;TCAP</div>
+              </div>
+            </Col>
+          </Row>
+          <div className="button-to-app">
+            <a href={appEndpoint} rel="noopener noreferrer" className="button-pink main-button main-button-link" target="_blank">
+              Go to App
+            </a>
+          </div>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={6} className="image">          
+          <img src={image} alt="Main" className="main-image" />          
+        </Col>      
+    </div>
   )
 }
 
