c4b453fb3629d29463c884dd5cff7a6e8a7108b6
jdestephen <jdestephen07@gmail.com>
2021-09-20 15:59:51 -0600

    copy post link

--- src/components/ShareSocial.js
+++ src/components/ShareSocial.js
@@ -28,18 +28,36 @@ const ShareSocial = (props) => {
     return `${siteUrl}${shareSlug}`;
   }
 
+  const copyCodeToClipboard = (e) => {
+    e.preventDefault();
+    // Create new element
+    const el = document.createElement("textarea");
+    // Set value (string to be copied)
+    el.value = shareUrl();
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute("readonly", "");
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand("copy");
+    // Remove temporary element
+    document.body.removeChild(el);
+  };
+
+
   return (
     <div className="share-post">
       <div className="share-caption">
         <h6>Share with</h6>        
       </div>
       <div className="share-buttons">
+        <button onClick={copyCodeToClipboard}>
+          <img src="/copy.svg" alt="Discord" />
+        </button>
         <RedditShareButton url={shareUrl()} title={title}>
           <img src="/Reddit.svg" alt="Reddit" />
         </RedditShareButton>
-        <button>
-          <img src="/Discord.svg" alt="Discord" />
-        </button>  
         <TwitterShareButton url={shareUrl()} title={title} hashtags={tags}>
           <img src="/Twitter.svg" alt="Twitter" />
         </TwitterShareButton>
