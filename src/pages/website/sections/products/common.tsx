c6097c8d99684eec804d4fdae3ab77bf5b791c5e
jdestephen <jdestephen07@gmail.com>
2024-05-22 18:44:23 -0600

    css updates

--- src/pages/website/sections/products/common.tsx
+++ src/pages/website/sections/products/common.tsx
@@ -3,13 +3,10 @@ import { Stack } from "react-bootstrap"
 import { BsCheckCircle } from "react-icons/bs";
 
 
-const StatBox = ({ title, value, divider }: { title: string, value: string, divider?: boolean }) => {
+const TotalBox = ({ title, value, divider }: { title: string, value: string, divider?: boolean }) => {
   return (
     <>
-      <Stack
-        direction="vertical"
-        style={{ width: "50%", alignItems: "center", justifyContent: "center", backgroundColor: "#090909" }}
-      >
+      <Stack direction="vertical" className="total-box" >
         <span
           style={{
             fontSize: "1.7rem",
@@ -22,7 +19,7 @@ const StatBox = ({ title, value, divider }: { title: string, value: string, divi
         <h3 style={{ color: "#A9A7BE" }} >{title}</h3>
       </Stack>
       {divider && (
-        <div style={{ height: "100%", borderLeft: "1px solid rgba(33, 33, 56, 0.70)" }} />
+        <div className="divider" />
       )}
     </>  
   )
@@ -37,17 +34,7 @@ type InfoCardProps = {
 
 export const ProductInfoCard = ({ headline, highlights, totals }: InfoCardProps) => {
   return (
-    <Stack
-      direction="vertical"
-      gap={2}
-      style={{
-        padding: "1.5rem 0rem 0rem 0rem",
-        backgroundColor: "#090909",
-        border: "0.5px solid rgba(33, 33, 56, 0.50)",
-        borderRadius: "5px",
-        alignItems: "space-between",
-      }}
-    >
+    <Stack direction="vertical" gap={2} className="product-info-card">
       <Stack
         direction="vertical"
         style={{
@@ -55,7 +42,7 @@ export const ProductInfoCard = ({ headline, highlights, totals }: InfoCardProps)
           width: "100%",
         }}
       >
-        <h2 style={{ color: "#d5d4e3", fontSize: "1.75rem" }}>
+        <h2 style={{ fontSize: "1.75rem" }}>
           {headline}
         </h2>
         <Stack
@@ -83,9 +70,14 @@ export const ProductInfoCard = ({ headline, highlights, totals }: InfoCardProps)
           borderTop: "0.5px solid rgba(33, 33, 56, 0.70)",
         }}
       >
-        {totals.map((total, index) => {
-          return <StatBox key={index} title={total.title} value={total.value} divider={index < totals.length - 1} />
-        })}
+        {totals.map((total, index) => (
+          <TotalBox
+            key={index}
+            title={total.title}
+            value={total.value}
+            divider={index < totals.length - 1}
+          />
+        ))}
       </Stack>
     </Stack>
   );
